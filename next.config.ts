b1ba2a32a726503a8229900d16acfe334183c6aa
Prathamesh Nimkar <zekhram007@gmail.com>
2026-09-12 13:05:07 +0530

    about.html redirect

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         destination: '/',
         permanent: true,
       },
+      {
+        source: '/about.html',
+        destination: '/about',
+        permanent: true,
+      },
     ]
   },
 };
